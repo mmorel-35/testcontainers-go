b3581fc87e20a2a08d58314ae9ac314b23df9c7b
Matthieu MOREL <matthieu.morel35@gmail.com>
2024-01-16 21:30:00 +0100

    Update main_test.go

--- modulegen/main_test.go
+++ modulegen/main_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/testcontainers/testcontainers-go/modulegen/internal"
 	"github.com/testcontainers/testcontainers-go/modulegen/internal/context"
