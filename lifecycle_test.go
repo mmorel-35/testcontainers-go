5d1ea13c4d0341435561f20233ba82b471ac149a
Matthieu MOREL <matthieu.morel35@gmail.com>
2024-01-16 21:56:33 +0100

    Update lifecycle_test.go

--- lifecycle_test.go
+++ lifecycle_test.go
@@ -209,7 +209,7 @@ func TestPreCreateModifierHook(t *testing.T) {
 		net, err := provider.CreateNetwork(ctx, NetworkRequest{
 			Name: networkName,
 		})
-		require.Nil(t, err)
+		require.NoError(t, err)
 		defer func() {
 			err := net.Remove(ctx)
 			if err != nil {
@@ -220,7 +220,7 @@ func TestPreCreateModifierHook(t *testing.T) {
 		dockerNetwork, err := provider.GetNetwork(ctx, NetworkRequest{
 			Name: networkName,
 		})
-		require.Nil(t, err)
+		require.NoError(t, err)
 
 		req := ContainerRequest{
 			Image:    nginxAlpineImage, // alpine image does expose port 80
@@ -238,7 +238,7 @@ func TestPreCreateModifierHook(t *testing.T) {
 		inputNetworkingConfig := &network.NetworkingConfig{}
 
 		err = provider.preCreateContainerHook(ctx, req, inputConfig, inputHostConfig, inputNetworkingConfig)
-		require.Nil(t, err)
+		require.NoError(t, err)
 
 		// assertions
 
