21d2c120b573e5ff948e20544ac83e85dee5aa41
Matthieu MOREL <matthieu.morel35@gmail.com>
2024-01-18 19:58:10 +0100

    Update cockroachdb_test.go

--- examples/cockroachdb/cockroachdb_test.go
+++ examples/cockroachdb/cockroachdb_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 // Task represents a unit of work to complete. We're going to be using this in
